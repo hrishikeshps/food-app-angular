4d5cdf033c54d1dfb9f3f9dec39e238c293f5bb2
Hrishikesh <hrishikeshps@gmail.com>
2023-12-20 23:29:33 +0530

    components created and routing defined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { RestaurantItemsComponent } from './pages/restaurant-items/restaurant-items.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoriesComponent,
+    RestaurantItemsComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
