3ca05d2916bde04c1fe01353c9ae006afa510c5b
Hrishikesh <hrishikeshps@gmail.com>
2023-12-22 00:19:31 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './layout/layout.module';
@@ -20,6 +21,7 @@ import { CreateOrderComponent } from './pages/create-order/create-order.componen
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     LayoutModule
   ],
--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { MainService } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  public categories: any = [];
+
+  constructor(private mainSrv: MainService){
+
+  }
+
+  ngOnInit(): void {
+    this.fetchAllCategories();
+  }
+
+  fetchAllCategories(){
+    this.mainSrv.getAllCategories()
+      .pipe(map((item: any) => {
+        // Check if categoryName is a string, and update it if needed
+        if (typeof item.categoryName === 'string') {
+          // Assuming you want to remove the string, you can set it to an empty string or null
+          item.categoryName = 'sd';
+        }
+        return item;
+      }))
+      .subscribe((res: any) => {
+      console.log(res);
+      this.categories = res.data;
+    });
+  }
 
 }
