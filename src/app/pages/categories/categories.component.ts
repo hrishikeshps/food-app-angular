8178b3f76cfcb250f3a12fd171fae5babd3a05ae
Hrishikesh <hrishikeshps@gmail.com>
2023-12-22 01:03:44 +0530

    adding logo

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -20,17 +20,10 @@ export class CategoriesComponent implements OnInit {
 
   fetchAllCategories(){
     this.mainSrv.getAllCategories()
-      .pipe(map((item: any) => {
-        // Check if categoryName is a string, and update it if needed
-        if (typeof item.categoryName === 'string') {
-          // Assuming you want to remove the string, you can set it to an empty string or null
-          item.categoryName = 'sd';
-        }
-        return item;
-      }))
       .subscribe((res: any) => {
-      console.log(res);
-      this.categories = res.data;
+        console.log(res);
+        this.categories = res.data.filter((item: any) => item.categoryName != 'string');
+        console.log(this.categories)
     });
   }
 
