cd9789b19f6a33d9d5d0d442a532023089f95817
Hrishikesh <hrishikeshps@gmail.com>
2023-12-26 23:52:44 +0530

    restaurent fetch menu items

--- src/app/pages/restaurant-items/restaurant-items.component.ts
+++ src/app/pages/restaurant-items/restaurant-items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-restaurant-items',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./restaurant-items.component.scss']
 })
 export class RestaurantItemsComponent {
-
+  constructor(private activatedRoute: ActivatedRoute){
+    this.activatedRoute.params.subscribe((res: any) => {
+      console.log(res)
+    })
+  }
 }
