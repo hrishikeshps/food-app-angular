a37986dde36e1c644709f95e177a66f9a9a4f789
Hrishikesh <hrishikeshps@gmail.com>
2023-12-28 22:19:36 +0530

    changes in layout

--- src/app/layout/components/card-view/card-view.component.ts
+++ src/app/layout/components/card-view/card-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-view',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-view.component.scss']
 })
 export class CardViewComponent {
+  @Input() cardData: any;
+
+  constructor(){
+
+  }
+  
+  ngOnInit(){
+    console.log(this.cardData);
+    
+  }
 
 }
